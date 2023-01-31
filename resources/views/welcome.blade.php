ca4c60d50249d42952ba9e051b4214dc9a1dbd32
iboonkhet@gmail.com <123219570+Rkknoob1@users.noreply.github.com>
2023-01-31 13:34:47 +0700

    update
    update

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,540 @@
+
+<!DOCTYPE html>
+<html lang="en" class="en">
+
+<head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+
+	<script async src="https://www.google-analytics.com/analytics.js"></script>
+
+	<link rel="profile" href="https://gmpg.org/xfn/11" />
+
+
+	<!--[if lt IE 9]>
+	<script src="https://nipponia-kosuge.jp/wp/wp-content/themes/kosuge_v2/lib/html5shiv.min.js"></script>
+	<![endif]-->
+
+	<meta name="keywords" content="小菅 源流の村,小菅村,山梨県,ホテル,多摩川,700人の村がひとつのホテルに。700人,NIPPONIA,KOSUGE,Hotel">
+	<link rel="shortcut icon" href="https://nipponia-kosuge.jp/wp/wp-content/themes/kosuge_v2/assets/images/favicon.ico">
+	<meta name="description" content="多摩川源流、700人の村が一つのホテルに。多摩川源流に位置する小菅村は、東京都心から車で約2時間。面積の95％が森林と豊かな自然に囲まれた人口約700人の小さな村です。日本の原風景が多く残るNIPPONIA（ニッポニア）小菅 源流の村で自然に寄り添う暮らしを体感する。">
+	<meta property="og:title" content="NIPPONIA（ニッポニア）小菅 源流の村｜700人の村がひとつのホテルに。分散型古民家ホテル">
+	<meta property="og:description" content="多摩川源流、700人の村が一つのホテルに。多摩川源流に位置する小菅村は、東京都心から車で約2時間。面積の95％が森林と豊かな自然に囲まれた人口約700人の小さな村です。日本の原風景が多く残るNIPPONIA（ニッポニア）小菅 源流の村で自然に寄り添う暮らしを体感する。">
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="https://nipponia-kosuge.jp/en">
+	<meta property="og:image" content="https://nipponia-kosuge.jp/wp/wp-content/themes/kosuge_v2/assets/images/og.jpg">
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="630" />
+
+	<meta name="twitter:card" content="photo">
+		<meta name="twitter:title" content="NIPPONIA（ニッポニア）小菅 源流の村｜700人の村がひとつのホテルに。分散型古民家ホテル">
+	<meta name="twitter:image:src" content="https://nipponia-kosuge.jp/wp/wp-content/themes/kosuge_v2/assets/images/og.jpg">
+	<meta name="twitter:domain" content="nipponia-kosuge.jp">
+	<meta property="fb:app_id" content="322262362512936">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1">
+
+	<title>หนองหินมัลเบอร์รี่เฮาส์ ท่องเที่ยวชุมชนบ้านหนองหิน</title>
+
+
+	<link rel="stylesheet" href="https://nipponia-kosuge.jp/wp/wp-content/themes/kosuge_v2/assets/css/main.css?220606a" type="text/css" />
+
+
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<script>
+		var page = "dining";
+		var siteURL = "https://nipponia-kosuge.jp/en/";
+	</script>
+
+	<script type="text/javascript" src="https://nipponia-kosuge.jp/wp/wp-content/themes/kosuge_v2/assets/js/bundle.js?200730a"></script>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Kanit">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</head>
+
+<!--  -->
+<style>
+    .ba{
+      /* background-color: green !important; */
+      background-color: #183618 !important;
+    }
+
+    #ba1{
+
+        background-color:  #192619;
+    }
+
+
+    body {
+        font-family: 'Kanit', sans-serif;
+      }
+
+
+
+    .cards-list {
+      z-index: 0;
+      width: 100%;
+      display: flex;
+      justify-content: space-around;
+      flex-wrap: wrap;
+    }
+
+    .card {
+      margin: 30px auto;
+      /* width: 290px;
+      height: 290px; */
+      border-radius: 40px;
+    /* box-shadow: 5px 5px 30px 7px rgba(0,0,0,0.25), -5px -5px 30px 7px rgba(0,0,0,0.22); */
+      cursor: pointer;
+      transition: 0.4s;
+    }
+
+    .card .card_image {
+      width: inherit;
+      height: inherit;
+      border-radius: 40px;
+    }
+
+    .card .card_image img {
+      width: inherit;
+      height: inherit;
+      border-radius: 40px;
+      object-fit: cover;
+    }
+
+    .card .card_title {
+      text-align: center;
+      border-radius: 0px 0px 40px 40px;
+      font-family: sans-serif;
+      font-weight: bold;
+      font-size: 30px;
+      margin-top: -80px;
+      height: 40px;
+    }
+
+    .card:hover {
+      transform: scale(0.9, 0.9);
+      box-shadow: 5px 5px 30px 15px rgba(0,0,0,0.25),
+        -5px -5px 30px 15px rgba(0,0,0,0.22);
+    }
+
+    .title-white {
+      color: white;
+    }
+
+    .title-black {
+      color: black;
+    }
+
+    @media all and (max-width: 500px) {
+      .card-list {
+        /* On small screens, we are no longer using row direction but column */
+        flex-direction: column;
+      }}
+    </style>
+
+
+<body class="en gallery dining">
+
+<header>
+	<a id="title" style="text-decoration:none" href="#">
+				NONG - HIN
+				<h1>หนองหิน <span style='margin-left: -0.2em'>มัลเบอร์รี่เฮาส์</span></h1>
+		<p> ท่องเที่ยวชุมชนบ้านหนองหิน</p>
+	</a>
+
+
+	<div id="top_foot" class="clearfix ba">
+		<div class="left">
+			<a href="https://www.instagram.com/nonghin.mulberry.house/" target="_blank" id="bt_schedule">
+									<div class="bt"></div><span class="ja">RESERVE</span>
+							</a>
+		</div>
+
+		<div class="right">
+			<ul>
+
+									<li
+										><a class="select en" href="#">หน้าหลัก</a></li>
+									<li
+										><a class=" en" href="#">เกี่ยวกับเรา</a></li>
+									<li
+										><a class="en" href="#">ข่าวสารและกิจกรรม</a></li>
+									<li
+										><a class=" en" href="#">ติดต่อเรา</a></li>
+
+
+							</ul>
+		</div>
+	</div>
+
+	<div class="rt_link clearfix">
+
+		<ul class="lang clearfix">
+			<li >
+				<a href="#" class="ja">
+                    EN
+                </a>
+            </li>
+			<li class="select">
+					TH
+            </li>
+		</ul>
+
+
+		<div class="line"></div>
+
+
+		<ul class="social clearfix">
+
+
+			<li>
+				<a href="https://www.instagram.com/nonghin.mulberry.house/" target="_blank">
+					<img src="https://nipponia-kosuge.jp/wp/wp-content/themes/kosuge_v2/assets/images/bt_in.svg" alt="Twitter" width="18">
+				</a>
+			</li>
+			<li>
+				<a href="https://www.facebook.com/NongHinMulberryHouse" target="_blank">
+					<img src="https://nipponia-kosuge.jp/wp/wp-content/themes/kosuge_v2/assets/images/bt_fb.svg" alt="Facebook" width="18">
+				</a>
+			</li>
+		</ul>
+
+	</div>
+
+</header>
+
+<div id="main">
+
+	<div id="movie_area">
+
+		<div id="top_title">
+			<h2>NONG-HIN </h2>
+					</div>
+
+		<div id="top_video_wrap">
+		<div class="gradient"></div>
+	<video class="full_resize" preload="none" loop="loop" muted="muted" playsinline="" preload="auto"
+
+  poster="img/หนองหินมัลเบอร์รี่เฮาส์/122809524_141499897664004_7844290338621171893_n.jpg">
+	<source src="http://127.0.0.1:8000/x1.mov" type="video/mp4">
+	</video>
+</div>
+
+	</div>
+
+
+	<div id="top_copy"> <h2>ผลิตภัณฑ์ของเรา</h2> </div>
+<br>
+
+<div style="margin: 30px;">
+  <div class="row align-items-start">
+    <div class="col-4">
+    <div class="card" >
+       <img src="img/หนองหินมัลเบอร์รี่เฮาส์/268802550_394644252349566_1235860328743203525_n.jpg"  style="border-radius: 8px;"  class="card-img-top" alt="..." >
+    </div>
+    </div>
+    <div class="col-4">
+    <div class="card">
+      <img src="img/หนองหินมัลเบอร์รี่เฮาส์/131572210_172285524585441_8007551273095612554_n.jpg"  style="border-radius: 8px;" class="card-img-top" alt="...">
+    </div>
+    </div>
+    <div class="col-4">
+    <div class="card">
+      <img src="img/หนองหินมัลเบอร์รี่เฮาส์/131942151_172285477918779_812946728730038904_n.jpg" style="border-radius: 8px;" class="card-img-top" alt="...">
+    </div>
+    </div>
+  </div>
+  <br>
+
+  <div class="row align-items-center">
+    <div class="col-4">
+     <div class="card">
+      <img src="img/หนองหินมัลเบอร์รี่เฮาส์/259243546_378032944010697_5596025837400353234_n.jpg" style="border-radius: 8px;" class="card-img-top" alt="...">
+    </div>
+    </div>
+    <div class="col-4">
+    <div class="card">
+      <img src="img/หนองหินมัลเบอร์รี่เฮาส์/316268655_615237240402006_4200553497091487669_n.jpg" style="border-radius: 8px;" class="card-img-top" alt="...">
+    </div>
+    </div>
+    <div class="col-4">
+    <div class="card">
+      <img src="img/หนองหินมัลเบอร์รี่เฮาส์/220424868_300472355100090_8684731401613327680_n.jpg" style="border-radius: 8px;" class="card-img-top" alt="...">
+    </div>
+    </div>
+  </div>
+  <br>
+  </div>
+</div>
+
+
+
+<div style="margin: 30px;">
+<div class="row">
+  <div class="col-sm-6">
+  <div id="top_copy"> <h3>สินค้าวางจำหน่าย</h3> </div><br>
+  <div class="row g-0">
+    <div class="col-md-3">
+      <img src="img/หนองหินมัลเบอร์รี่เฮาส์/268802550_394644252349566_1235860328743203525_n.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">น้ำผลไม้มลเบอร์รี่</h5>
+        <a class="text-dark" style="text-decoration:none" href=""> <p class="card-text">สั่งสินค้าชุมชนออนไลน์ โชห่วยการ์เด้น จัดส่งทั่วโลก</p></a>
+        <p class="card-text"><small class="text-muted">20 มกราคม 2023 </small></p>
+      </div>
+    </div>
+  </div><br>
+  <div class="row g-0">
+    <div class="col-md-3">
+      <img src="img/หนองหินมัลเบอร์รี่เฮาส์/294899782_523915889534142_8892917977013967565_n.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">เยลลี่มัลเบอร์รี่ผสมบุก </h5>
+        <a class="text-dark" style="text-decoration:none" href=""> <p class="card-text">สั่งสินค้าชุมชนออนไลน์ โชห่วยการ์เด้น จัดส่งทั่วโลก</p></a>
+        <p class="card-text"><small class="text-muted">20 มกราคม 2023 </small></p>
+      </div>
+    </div>
+  </div><br>
+  <div class="row g-0">
+    <div class="col-md-3">
+      <img src="img/หนองหินมัลเบอร์รี่เฮาส์/131572210_172285524585441_8007551273095612554_n.jpg" class="img-fluid rounded-start" alt="...">
+    </div><br>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">ชุดของชำร่วย</h5>
+        <a class="text-dark" style="text-decoration:none" href=""> <p class="card-text">ขายออนไลน์และมีขายหน้าร้าน Orderสินค้าชุมชนผ่านร้านค้าออนไลน์ โชห่วยการ์เด้น สั่งสินค้าเกษตรกร โชห่วยการ์เด้น </p></a>
+        <p class="card-text"><small class="text-muted">20 มกราคม 2023 </small></p>
+      </div>
+    </div>
+  </div>
+  </div>
+
+
+  <div class="col-sm-6">
+      <div class="card-body">
+
+       <div class="container text-center">
+        <div class="row">
+          <div class="col-6 col-sm-6">
+            <div class="card" >
+              <img src="img/หนองหินมัลเบอร์รี่เฮาส์/268802550_394644252349566_1235860328743203525_n.jpg"  style="border-radius: 8px;"  class="card-img-top" alt="..." >
+            </div>
+          </div>
+          <div class="col-6 col-sm-6">
+              <div class="card">
+                <img src="img/หนองหินมัลเบอร์รี่เฮาส์/131572210_172285524585441_8007551273095612554_n.jpg"  style="border-radius: 8px;" class="card-img-top" alt="...">
+              </div>
+        </div>
+
+          <!-- Force next columns to break to new line at md breakpoint and up -->
+          <div class="w-100 d-none d-md-block"></div>
+
+          <div class="col-6 col-sm-6">
+            <div class="card">
+              <img src="img/หนองหินมัลเบอร์รี่เฮาส์/131942151_172285477918779_812946728730038904_n.jpg" style="border-radius: 8px;" class="card-img-top" alt="...">
+            </div>
+          </div>
+          <div class="col-6 col-sm-6">
+            <div class="card">
+              <img src="img/หนองหินมัลเบอร์รี่เฮาส์/259243546_378032944010697_5596025837400353234_n.jpg" style="border-radius: 8px;" class="card-img-top" alt="...">
+            </div>
+          </div>
+        </div>
+      </div>
+
+      </div>
+  </div>
+
+  <div class="container text-center">
+    <button type="button" class="btn btn-outline-dark ">ผลิตภัณฑ์ทั้งหมด</button>
+  </div><br>
+
+</div>
+</div>
+
+
+<div style="margin: 100px;">
+<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+    <img src="img/หนองหินมัลเบอร์รี่เฮาส์/268074317_10224169629282347_4209744539854354381_n.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+    <img src="img/หนองหินมัลเบอร์รี่เฮาส์/267396699_10224169567440801_8924028543681636972_n.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+    <img src="img/หนองหินมัลเบอร์รี่เฮาส์/268282847_10224169627762309_7874773912122669896_n.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</div>
+
+
+<!-- <div id="ba1"> -->
+<div id="top_copy" class="text-dark text-center"> <br> <h2>กิจกรรมของเรา</h2> </div>
+<div style="margin: 30px;">
+  <div class="row align-items-start">
+    <div class="col-4">
+       <img src="img/หนองหินมัลเบอร์รี่เฮาส์/146242493_279718637085767_7713450872874514791_n.jpg" style="  border-radius: 10px;" class="card-img-top" alt="..." >
+    </div>
+    <div class="col-4">
+      <img src="img/หนองหินมัลเบอร์รี่เฮาส์/148163758_279716820419282_1095531325775940460_n.jpg" style="  border-radius: 10px;" class="card-img-top" alt="...">
+    </div>
+    <div class="col-4">
+     <img src="img/หนองหินมัลเบอร์รี่เฮาส์/145893358_279718367085794_8563530045114907303_n.jpg" style="  border-radius: 10px;"  class="card-img-top" alt="...">
+    </div>
+  </div>
+  <br>
+
+  <div class="container text-center">
+  <button type="button" class="btn btn-outline-dark">กิจกรรมทั้งหมด</button>
+  </div><br>
+
+</div>
+<!-- </div> -->
+
+
+<br>
+<div style="margin: 30px;">
+<div class="row row-cols-1 row-cols-md-4 g-5">
+  <div class="col">
+    <div class="card">
+      <img src="img/หนองหินมัลเบอร์รี่เฮาส์/326801828_975747950055613_8619080426953443856_n.jpg" class="card-img-top" alt="...">
+      </div>
+      <div class="">
+        <small class="text-secondary">27 มกราคาม 2023</small>
+        <p class="card-text">ศูนย์ความร่วมมือกับภาคอุตสาหกรรม มหาวิทยาลัยมหาสารคาม ถ่ายทอดเทคโนโลยีให้กับผู้ประกอบการ</p>
+      </div>
+  </div>
+  <div class="col">
+  <div class="card">
+      <img src="img/หนองหินมัลเบอร์รี่เฮาส์/316529013_615237273735336_4693721747319186163_n.jpg" class="card-img-top" alt="...">
+      </div>
+      <div class="">
+        <small class="text-secondary">21 พฤศจิการยน 2022</small>
+        <p class="card-text">พาน้องมัลลี่ กลับมหาสารคาม "หนองหินมัลเบอร์รี่เฮาส์"</p>
+      </div>
+  </div>
+  <div class="col">
+  <div class="card">
+      <img src="img/หนองหินมัลเบอร์รี่เฮาส์/315978082_612381824020881_7313054687757158636_n.jpg" class="card-img-top" alt="...">
+      </div>
+      <div class="">
+        <small class="text-secondary">17 มกราคาม 2023</small>
+        <p class="card-text">“น้องมัลลี่”เป็นตัวแทนมาออกบูช ที่ อ.ต.ก.</p>
+      </div>
+  </div>
+  <div class="col">
+  <div class="card">
+      <img src="img/หนองหินมัลเบอร์รี่เฮาส์/278473075_469456731534984_1384400115950252875_n.jpg" class="card-img-top" alt="...">
+      </div>
+      <div class="">
+        <small class="text-secondary">20 เมษายน 2022</small>
+        <p class="card-text">หนองหินมัลเบอร์รี่😊 ขอแสดงความยินดีกับน้องๆ มมส. IT , THM ที่เรียนจบและ รับพระราชทานปริญญาบัตร ในวันที่ 21เมษายน65นี้</p>
+      </div>
+  </div>
+</div>
+
+  <div class="container text-center">
+    <button type="button" class="btn btn-outline-dark ">ข่าวสารทั้งหมด</button>
+  </div><br>
+
+</div>
+
+
+<footer class="" id="ba1">
+
+	<div class="top clearfix">
+
+		<div class="left">
+			<a href="https://nipponia-kosuge.jp/en/">
+				<img src="https://nipponia-kosuge.jp/wp/wp-content/themes/kosuge_v2/assets/images/logo_nipponia_white.svg" alt="NONG-HIN" width="129">
+				<h5>หนองหินมัลเบอร์รี่เฮาส์ </h5>
+			</a>
+
+
+			<ul class="social clearfix">
+				<li>
+					<a href="https://www.instagram.com/nonghin.mulberry.house/" target="_blank">
+						<img src="https://nipponia-kosuge.jp/wp/wp-content/themes/kosuge_v2/assets/images/bt_in.svg" alt="Twitter" width="18">
+					</a>
+				</li>
+
+				<li>
+					<a href="https://www.facebook.com/NongHinMulberryHouse" target="_blank">
+						<img src="https://nipponia-kosuge.jp/wp/wp-content/themes/kosuge_v2/assets/images/bt_fb.svg" alt="Facebook" width="18">
+					</a>
+				</li>
+			</ul>
+		</div>
+
+		<div class="center">
+
+						<div class="left">เบอร์โทร.</div>
+						<div class="right clearfix">
+								<div class="right">
+									<div class="small">063 716 7310</div>
+								</div>
+						</div><br>
+
+						<div class="left">ที่อยู่.</div>
+						<div class="right clearfix">
+							<a href="https://www.google.com/maps/place/%E0%B8%9A%E0%B9%89%E0%B8%B2%E0%B8%99%E0%B8%AB%E0%B8%99%E0%B8%AD%E0%B8%87%E0%B8%AB%E0%B8%B4%E0%B8%99+Tambon+Khok+Ko,+Amphoe+Mueang+Maha+Sarakham,+Chang+Wat+Maha+Sarakham+44000/@16.023085,103.2681709,17z/data=!3m1!4b1!4m5!3m4!1s0x31180eaac1fde3fb:0x2e8a360c12f36c57!8m2!3d16.023085!4d103.2681709" style="text-decoration:none" target="_blank" class="clearfix">
+
+								<div class="right text-light">
+									<div class="small">44000</div>
+									<div class="middle">บ้านหนองหิน หมู่บ้าน 89หมู่ 4 บ้านหนองหิน
+                                       <br> Khok Ko, Mueang Maha Sarakham District, Maha Sarakham 44000</div>
+									<div>(Google map)</div>
+								</div>
+							</a>
+						</div>
+		</div>
+	</div>
+
+	<div class="under clearfix">
+		<div class="left">
+			<ul class="clearfix">
+				<li><a href="https://docs.google.com/forms/d/e/1FAIpQLSdQvNfcQWf7taBu9TdJjB_DOTi9mKy_JCfgiXqnAK97s1wIOA/viewform?embedded=true" target="blank">Contact Us</a></li>
+				<li><a href="https://nipponia-kosuge.jp/en/company/">Company Overview</a></li>
+				<li><a href="https://nipponia-kosuge.jp/en/privacy/">Privacy Policy</a></li>
+			</ul>
+
+
+		</div>
+
+		<div class="right">Copyright © EDGE CO., LTD All Rights Reserved.</div>
+	</div>
+
+</footer>
+
+
+
+<div id="image_float">
+	<a id="bt_float_close"><span class="arrow"></span></a>
+	<a id="bt_float_right"><span class="arrow"></span></a>
+	<a id="bt_float_left"><span class="arrow"></span></a>
+	<div class="copyright"><div>Copyright © EDGE CO., LTD All Rights Reserved.</div></div>
+	<div class="image"><p></p></div>
+	<div class="bg"></div>
+</div>
+</body>
+</html>
+
